2fc2620eaa6c00354e5b91e21a9dc0187ec6a18f
ronanrocking <ronanrocking@gmail.com>
2026-05-26 19:46:43 +0530

    Multiple Louvain final

--- main.py
+++ main.py
@@ -90,11 +90,11 @@ def get_consensus_scaffold(graph, algo_type, n_runs=15):
     return consensus_communities
 
 # --- Main Execution Setup ---
-#dataset_list = ["acm", "amac", "amap", "citeseer", "cocs", "cora", "film", "pubmed", "uat"]
-dataset_list = ["cocs", "pubmed"]
+dataset_list = ["acm", "amac", "amap", "citeseer", "cocs", "cora", "film", "pubmed", "uat"]
+#dataset_list = ["cocs", "pubmed"]
 device = torch.device('cpu')
 b = 0.001 # Modularity loss weight from paper
-file_name = "consensus_results.csv"
+file_name = "consensus_results_again.csv"
 
 for ds in dataset_list:
     args.dataset = ds
